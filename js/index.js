87c8107dd072e60ab4d77f66e167dcf0c99e63d0
yaljamal <yazan.aljamal1@gmail.com>
2020-02-05 00:22:28 +0200

    add some edit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var name = prompt("Whats Ur Name ?");
+var hobies=prompt("Whats Ur Hobies ?");
+var phone_number = prompt("Whats Ur Phone Number ?");
+
+if (phone_number==123)
+{
+    document.write(`<h1>`+ name + +   + hobies + + `<h1>`);
+}
+
+
+
